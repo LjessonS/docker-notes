be54af4ff45266544a6fe8de9ca4c501f5d61734
LjessonS <ltww2274@163.com>
2019-01-31 09:34:15 +0800

    add mysql Dockerfile

---
+++ mysql/rm_docker.sh
@@ -0,0 +1,4 @@
+docker stop test_ub_mysql
+docker rm test_ub_mysql
+docker rmi ub_mysql
+
---
+++ mysql/ssh.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/expect -f
+
+set port 10024
+set user root
+set host 172.16.56.2
+set password pwd
+set timeout 30
+
+spawn ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p $port $user@$host
+expect "*assword:*"
+
+send "$password\r"
+interact
